a160daf96d8e940f499872d93434410f3751325b
jcvar <36166613+jcvar@users.noreply.github.com>
2020-04-19 18:56:45 -0500

    Finished homework 3

--- tarea3_jcvargasq_savargasqu/tarea3_jcvargasq_savargasqu.ino
+++ tarea3_jcvargasq_savargasqu/tarea3_jcvargasq_savargasqu.ino
@@ -34,15 +34,19 @@ enum traffic_state_t {
 };
 
 // Traffic light duration
-const int dG  = 5000;
+const int dG  = 4000;
 const int dY  = 2000;
 const int dR  = 7000;
-const int dRY = 1000;
+const int dRY = 2000;
 
 // Walk lights state
 enum walk_state_t {
-	dont, walk_on, walk_off
+	sDont, sWalk_on, sWalk_off
 };
+
+// Walk light duration
+const int w_on  = 250;
+const int w_off = 250;
 
 // Size of circle representing the LED
 #define light_size    16
@@ -52,68 +56,107 @@ enum walk_state_t {
 
 // Sketch variables
 traffic_state_t traffic_light = sG;
+walk_state_t walk_light = sDont;
 
 // Turn off all LED circles except for one. Pass color as argument
 void draw_lights(traffic_state_t light) {
 	static traffic_state_t old_light = sRY;
-	if (light != old_light){
-		old_light = light;
-		// Reset all lights
-		for (int i = 0; i < 3; i++) {
-			TFTscreen.fillCircle(light_offsetx, i*light_pos + light_offsety,  light_size, K);
-			TFTscreen.drawCircle(light_offsetx, i*light_pos + light_offsety, light_size, light_colors[i]);
-		}
-		
-		// Light up specific lights
-		if(light == sR || light == sRY){
-			TFTscreen.fillCircle(light_offsetx, iR*light_pos + light_offsety, light_size, light_colors[iR]);
-		}
-		if(light == sRY || light == sY){
-			TFTscreen.fillCircle(light_offsetx, iY*light_pos + light_offsety, light_size, light_colors[iY]);
-		}
-		if(light == sG){
-			TFTscreen.fillCircle(light_offsetx, iG*light_pos + light_offsety, light_size, light_colors[iG]);
-		}
+	
+	// Reset all lights
+	for (int i = 0; i < 3; i++) {
+		TFTscreen.fillCircle(light_offsetx, i*light_pos + light_offsety,  light_size, K);
+		TFTscreen.drawCircle(light_offsetx, i*light_pos + light_offsety, light_size, light_colors[i]);
+	}
+	
+	// Light up specific lights
+	if(light == sR || light == sRY){
+		TFTscreen.fillCircle(light_offsetx, iR*light_pos + light_offsety, light_size, light_colors[iR]);
+	}
+	if(light == sRY || light == sY){
+		TFTscreen.fillCircle(light_offsetx, iY*light_pos + light_offsety, light_size, light_colors[iY]);
+	}
+	if(light == sG){
+		TFTscreen.fillCircle(light_offsetx, iG*light_pos + light_offsety, light_size, light_colors[iG]);
 	}
 }
 
-void draw_walk() {
-	TFTscreen.stroke(O);
-	TFTscreen.text("WALK", 87, 67);
-	TFTscreen.text("DONT", 87, 47);
+void draw_walk(walk_state_t light) {
+	static walk_state_t old_light = sWalk_off;
 	
+	if(light == sDont){
+		TFTscreen.stroke(O);
+		TFTscreen.text("WALK", 87, 67);
+		TFTscreen.text("DONT", 87, 47);
+	}
+	if(light == sWalk_on){
+		TFTscreen.stroke(K);
+		TFTscreen.text("DONT", 87, 47);
+		TFTscreen.stroke(G);
+		TFTscreen.text("WALK", 87, 67);
+	}
+	if(light == sWalk_off){
+		TFTscreen.stroke(K);
+		TFTscreen.text("WALK", 87, 67);
+	}
 }
 
 void traffic_state_machine(){
-	static unsigned long prev_millis = millis();
+	static unsigned long traffic_millis = millis();
 	switch(traffic_light){
 		case sG:
-			if((millis() - prev_millis) >= dG){
-				prev_millis = millis();
+			if((millis() - traffic_millis) >= dG){
+				traffic_millis = millis();
 				traffic_light = sY;
+				draw_lights(traffic_light);
 			}
-		break;
-
+			break;
+			
 		case sY:
-			if((millis() - prev_millis) >= dY){
-				prev_millis = millis();
+			if((millis() - traffic_millis) >= dY){
+				traffic_millis = millis();
 				traffic_light = sR;
+				draw_lights(traffic_light);
+				walk_light = sWalk_on;
+				draw_walk(walk_light);
 			}
-		break;
-
+			break;
+			
 		case sR:
-			if((millis() - prev_millis) >= dR){
-				prev_millis = millis();
+			static unsigned long walk_millis = millis();
+			switch(walk_light){
+				case sWalk_on:
+					if((millis() - walk_millis) >= w_on){
+						walk_millis = millis();
+						walk_light = sWalk_off;
+						draw_walk(walk_light);
+					}
+					break;
+					
+				case sWalk_off:
+					if((millis() - walk_millis) >= w_off){
+						walk_millis = millis();
+						walk_light = sWalk_on;
+						draw_walk(walk_light);
+					}
+					break;
+			}
+			
+			if((millis() - traffic_millis) >= dR){
+				traffic_millis = millis();
 				traffic_light = sRY;
+				draw_lights(traffic_light);
+				walk_light = sDont;
+				draw_walk(walk_light);
 			}
-		break;
-
+			break;
+			
 		case sRY:
-			if((millis() - prev_millis) >= dRY){
-				prev_millis = millis();
+			if((millis() - traffic_millis) >= dRY){
+				traffic_millis = millis();
 				traffic_light = sG;
+				draw_lights(traffic_light);
 			}
-		break;
+			break;
 	}
 }
 
@@ -128,10 +171,11 @@ void setup() {
 		TFTscreen.drawRoundRect(20, 4+40*i, 41, 41, 4, W);
 	}
 	TFTscreen.drawRoundRect(80, 40, 60, 48, 8, W);
+	
+	draw_lights(traffic_light);
+	draw_walk(walk_light);
 }
 
 void loop() {
-	draw_lights(traffic_light);
-	//draw_walk();
 	traffic_state_machine();
 }
