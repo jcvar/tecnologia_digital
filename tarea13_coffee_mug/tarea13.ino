27b046669666b919fc03e95520ffc944f35d6377
jcvar <36166613+jcvar@users.noreply.github.com>
2020-06-03 13:22:27 -0500

    Init tarea13.ino

---
+++ tarea13_coffee_mug/tarea13.ino
@@ -0,0 +1,23 @@
+/*
+Tarea 13
+Coffee Mug
+
+Juan Camilo Vargas Q.
+Sergio Alejandro Vargas Q.
+
+Tecnologia Digital
+Universidad Nacional de Colombia
+2020-06-03
+*/
+#include <TFT.h>
+#include "coffee_mug.h"
+
+
+
+void setup(){
+}
+
+void loop(){
+}
+
+
