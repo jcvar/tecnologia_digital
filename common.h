4e105ccd6f55c2a6e04b58d5b8a4013377f37c42
Sergio Alejandro Vargas <savargasqu@unal.edu.co>
2020-06-08 15:24:03 -0500

    moved common.h and refactored pong

---
+++ common.h
@@ -0,0 +1,34 @@
+// COMMON DEFINITIONS
+#include <TFT.h>
+#include <SPI.h>
+#include <avr/pgmspace.h>
+
+// TFT DEFINITIONS
+#define cs  10
+#define dc   9
+#define rst  8
+
+// 16-bit ('565') colors
+#define COLOR_BLACK 0x0000
+#define COLOR_WHITE 0xFFFF
+#define COLOR_RED 0xF800
+#define COLOR_GREEN 0x07E0
+#define COLOR_BLUE 0x001F
+#define COLOR_CYAN 0x07FF
+#define COLOR_MAGENTA 0xF81F
+#define COLOR_YELLOW 0xFFE0
+#define COLOR_ORANGE 0xFC00
+
+/// Court parameters
+#define COURT_X 5
+#define COURT_Y 10
+#define COURT_W 150
+#define COURT_H 108
+
+// Court structure
+typedef struct {
+	int top;
+	int bottom;
+	int left;
+	int right;
+} court_t;
--- examples_ftp/2020-06-08_pong/Pong/PongClase.ino
+++ examples_ftp/2020-06-08_pong/Pong/PongClase.ino
@@ -1,42 +1,18 @@
-#include <TFT.h>  // Arduino LCD library
-#include <SPI.h>
 
-#include <pgmspace.h>
+// Ejercicio: Crear segunda paleta
 
-// pin definition for the Uno
-#define cs   10
-#define dc   9
-#define rst  8
-
-// pin definition for the Leonardo
-// #define cs   7
-// #define dc   0
-// #define rst  1
-
-TFT TFTscreen = TFT(cs, dc, rst);
-
-// Some ready-made 16-bit ('565') color settings:
-#define COLOR_BLACK 0x0000
-#define COLOR_WHITE 0xFFFF
-#define COLOR_RED 0xF800
-#define COLOR_GREEN 0x07E0
-#define COLOR_BLUE 0x001F
-#define COLOR_CYAN 0x07FF
-#define COLOR_MAGENTA 0xF81F
-#define COLOR_YELLOW 0xFFE0
-#define COLOR_ORANGE 0xFC00
+#include "common.h"
 
 #include "images.h"
 #include "ball.h"
 #include "paddle.h"
 
+TFT TFTscreen = TFT(cs, dc, rst);
+court_t court;
+
+
 // variables for the position of the ball and paddle
 int paddleY = 0;
-
-int courtX=5,courtY=10,courtW=150,courtH=108;
-
-int courtTopLine=0,courtBottomLine=0,courtLeftLine=0,courtRightLine=0;
-
 Ball myBall(20,33,2,3);
 Paddle myPaddleRight(145,0,1,200);
 
@@ -63,15 +39,14 @@ void setup() {
 	
 	TFTscreen.drawRect(courtX, courtY, courtW,courtH,0xF800);
 	
-	courtTopLine=courtY;
-	courtBottomLine=courtY+courtH;
-	courtLeftLine=courtY;
-	courtRightLine=courtX+courtW;
-	
+	// Court initialization
+	court.top = COURT_Y;
+	court.bottom = COURT_Y + COURT_H;
+	court.left = COURT_X;
+	court.right = COURT_X + COURT_W;
 }
 
 void loop() {
-	
 	TFTscreen.drawRect(5, 10, 150, 108,0xF800);
 	
 	// map the potentiometer value to the position of paddle's location
@@ -88,7 +63,4 @@ void loop() {
 	delay(10);
 	
 }
-
-
-
 
