c5cb29b54c829d79656904ccb5fda4b03624ef18
jcvar <36166613+jcvar@users.noreply.github.com>
2020-06-16 23:31:22 -0500

    Update Pong

--- tarea15_pong/tarea15.ino
+++ tarea15_pong/tarea15.ino
@@ -19,10 +19,21 @@ TFT TFTscreen = TFT(CS, DC, RST);
 #define MILLIS_BALL 50
 #define MILLIS_PADDLE 50
 
+#define BALL_W 5
+#define BALL_H 5
+#define PADDLE_W 5
+#define PADDLE_H 20
 
-court_t court = {COURT_Y, COURT_Y + COURT_H, COURT_X, COURT_X + COURT_W};
+#define PIN_LEFT A0
+#define PIN_RIGHT A1
 
-//Ball ball = new Ball 
+court_t court = {COURT_Y + 1, COURT_Y + COURT_H - 1, COURT_X + 1, COURT_X + COURT_W - 1};
+
+// Ball(pX, pY, dX, dY);
+Ball ball(80, 64, 1, 1);
+// Paddle(pX, pY);
+Paddle left(court.left, court.top);
+Paddle right(court.right - PADDLE_W, court.top);
 
 void setup() {
 	// Initialize screen
@@ -34,10 +45,20 @@ void setup() {
 
 void loop() {
 	static unsigned long millis_ball = 0;
-	static unsigned long millis_paddle = 0;
-
+	static unsigned long millis_left = 0;
+	static unsigned long millis_right = 0;
+	
 	if(millis() - millis_ball > MILLIS_BALL){
-		
+		millis_ball = millis();
+		ball.move();
+	}
+	if(millis() - millis_left > MILLIS_PADDLE){
+		millis_left = millis();
+		left.move(map(analogRead(PIN_LEFT), 1023, 0, court.top, court.bottom - PADDLE_H));
+	}
+	if(millis() - millis_right > MILLIS_PADDLE){
+		millis_right = millis();
+		right.move(map(analogRead(PIN_RIGHT), 1023, 0, court.top, court.bottom - PADDLE_H));
 	}
 }
 
