86d4d7093ae71b49e6ecc074e6d15b94a23a5bb3
jcvar <36166613+jcvar@users.noreply.github.com>
2020-07-01 15:47:41 -0500

    Display mode on screen

--- tarea16_clock/tarea16_clock.ino
+++ tarea16_clock/tarea16_clock.ino
@@ -39,120 +39,122 @@ void update_time();//(unsigned*, unsigned*);
 
 
 void setup() {
-  TFTscreen.begin(INITR_BLACKTAB); // INITR_BLACKTAB for BGR
-  TFTscreen.background(COLOR_BLACK);
-  TFTscreen.drawRect(COURT_X, COURT_Y, COURT_W, COURT_H, COLOR_RED);
-
-  // External inits
-  Timer1.initialize(500000);//timing for 500ms
-  Timer1.attachInterrupt(timer1_isr);//declare the interrupt serve routine:TimingISR
-  mode_button.begin();
-  next_button.begin();
-
-  force_draw(clk);
+	TFTscreen.begin(); // For simulator
+	//TFTscreen.begin(INITR_BLACKTAB); // INITR_BLACKTAB for BGR
+	TFTscreen.background(COLOR_BLACK);
+	TFTscreen.drawRect(COURT_X, COURT_Y, COURT_W, COURT_H, COLOR_RED);
+	
+	// External inits
+	Timer1.initialize(500000);//timing for 500ms
+	Timer1.attachInterrupt(timer1_isr);//declare the interrupt serve routine:TimingISR
+	mode_button.begin();
+	next_button.begin();
+	
+	force_draw(clk);
 } // END SETUP
 
 void loop() {
-  static unsigned long loop_millis = 0;
-
-  mode_button.read();
-  next_button.read();
-
-  if (millis() - loop_millis > MILLIS_LOOP) {
-    loop_millis = millis();
-
-    if (mode_button.wasPressed()) {
-      update_mode();
-    }
-    if (next_button.wasPressed()) {
-      update_next(clk_mode);
-    }
-
-  }
-
+	static unsigned long loop_millis = 0;
+	
+	mode_button.read();
+	next_button.read();
+	
+	if (millis() - loop_millis > MILLIS_LOOP) {
+		loop_millis = millis();
+		
+		if (mode_button.wasPressed()) {
+			update_mode();
+			draw_mode(clk_mode);
+		}
+		if (next_button.wasPressed()) {
+			update_next(clk_mode);
+		}
+		
+	}
+	
 } // END LOOP
 
 void timer1_isr() {
-  clk.active = !clk.active;
-  if (clk.active) {
-    update_time();
-    if (clk_mode != set_alarm_active &&
-        clk_mode != set_alarm_hour &&
-        clk_mode != set_alarm_minute) {
-      draw_time(clk);
-    }
-  }
+	clk.active = !clk.active;
+	if (clk.active) {
+		update_time();
+		if (clk_mode != set_alarm_active &&
+			clk_mode != set_alarm_hour &&
+		clk_mode != set_alarm_minute) {
+			draw_time(clk);
+		}
+	}
 }
 
 
 void update_mode() {
-  switch (clk_mode) {
-    case set_normal:
-      clk_mode = set_alarm_active;
-      break;
-    case set_alarm_active:
-      clk_mode = set_alarm_hour;
-      break;
-    case set_alarm_hour:
-      clk_mode = set_alarm_minute;
-      break;
-    case set_alarm_minute:
-      clk_mode = set_hour;
-      break;
-    case set_hour:
-      clk_mode = set_minute;
-      break;
-    case set_minute:
-      clk_mode = set_sec;
-      break;
-    case set_sec:
-      clk_mode = set_normal;
-      break;
-    default:
-      clk_mode = set_normal;
-      break;
-
-  }
+	switch (clk_mode) {
+		case set_normal:
+			clk_mode = set_alarm_active;
+			break;
+		case set_alarm_active:
+			clk_mode = set_alarm_hour;
+			break;
+		case set_alarm_hour:
+			clk_mode = set_alarm_minute;
+			break;
+		case set_alarm_minute:
+			clk_mode = set_hour;
+			break;
+		case set_hour:
+			clk_mode = set_minute;
+			break;
+		case set_minute:
+			clk_mode = set_sec;
+			break;
+		case set_sec:
+			clk_mode = set_normal;
+			break;
+		default:
+			clk_mode = set_normal;
+			break;
+			
+	}
 }
 
 void update_next(state_t mode) {
-  switch (mode) {
-    case set_normal:
-      // DO NOTHING
-      break;
-    case set_alarm_active:
-      alarm.active = !alarm.active;
-      break;
-    case set_alarm_hour:
-      alarm.hour = (alarm.hour + 1) % 24;
-      break;
-    case set_alarm_minute:
-      alarm.minute = (alarm.minute + 1) % 60;
-      break;
-    case set_hour:
-      clk.hour = (clk.hour + 1) % 24;
-      break;
-    case set_minute:
-      clk.minute = (clk.minute + 1) % 60;
-      break;
-    case set_sec:
-      clk.second = 0;
-      break;
-  }
+	switch (mode) {
+		case set_normal:
+			// DO NOTHING
+			break;
+		case set_alarm_active:
+			alarm.active = !alarm.active;
+			break;
+		case set_alarm_hour:
+			alarm.hour = (alarm.hour + 1) % 24;
+			break;
+		case set_alarm_minute:
+			alarm.minute = (alarm.minute + 1) % 60;
+			break;
+		case set_hour:
+			clk.hour = (clk.hour + 1) % 24;
+			break;
+		case set_minute:
+			clk.minute = (clk.minute + 1) % 60;
+			break;
+		case set_sec:
+			clk.second = 0;
+			break;
+	}
 }
 
 
 void update_time() {
-  clk.second++;
-  if (clk.second == 60) {
-    clk.second = 0;
-    clk.minute++;					// Update minute
-    if (clk.minute == 60) {
-      clk.minute = 0;
-      clk.hour++;					// Update hour
-      if (clk.hour == 24) {
-        clk.hour = 0;
-      }
-    }
-  }
+	clk.second++;
+	if (clk.second == 60) {
+		clk.second = 0;
+		clk.minute++;					// Update minute
+		if (clk.minute == 60) {
+			clk.minute = 0;
+			clk.hour++;					// Update hour
+			if (clk.hour == 24) {
+				clk.hour = 0;
+			}
+		}
+	}
 }
