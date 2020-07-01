b5acca9b7556eaed6784b5f943d18df35a323a87
jcvar <36166613+jcvar@users.noreply.github.com>
2020-07-01 14:16:53 -0500

    Add button logic

--- tarea16_clock/tarea16_clock.ino
+++ tarea16_clock/tarea16_clock.ino
@@ -26,7 +26,7 @@
 
 // Enum for global clock states
 typedef enum {
-  normal,
+  set_normal,
   set_alarm_active,
   set_alarm_hour,
   set_alarm_min,
@@ -55,11 +55,16 @@ Button next_button(5); // Cycle through time values in each state_t
 unsigned clk_hour = 0;
 unsigned clk_minute = 0;
 unsigned clk_second = 0;
-bool half = true;
-state_t mode = normal;
+bool clk_half = true;
+bool clk_alarm = false;
+unsigned clk_alarm_hour = 0;
+unsigned clk_alarm_minute = 0;
+state_t clk_mode = normal;
 
 
 /* DOCS */
+// TimerOne interrupt serve routine
+void timer1_isr();
 // Main clock logic
 void update_time();//(unsigned*, unsigned*);
 // Redraw time values if changed
@@ -92,20 +97,85 @@ void loop() {
   next_button.read();
 
   if (millis() - loop_millis > MILLIS_LOOP) {
-    
+    loop_millis = millis();
+
+    if (mode_button.wasPressed()) {
+      update_mode();
+    }
+    if (next_button.wasPressed()) {
+      update_next(clk_mode);
+    }
+
   }
 
 } // END LOOP
 
 void timer1_isr() {
-  half = !half;
+  clk_half = !clk_half;
 
-  if (half) {
+  if (clk_half) {
     update_time();
     draw_time(clk_hour, clk_minute, clk_second);
   }
 
 }
+
+void update_mode() {
+  switch (clk_mode) {
+    case set_normal:
+      clk_mode = set_alarm_active;
+      break;
+    case set_alarm_active:
+      clk_mode = set_alarm_hour;
+      break;
+    case set_alarm_hour:
+      clk_mode = set_alarm_min;
+      break;
+    case set_alarm_min:
+      clk_mode = set_hour;
+      break;
+    case set_hour:
+      clk_mode = set_min;
+      break;
+    case set_min:
+      clk_mode = set_sec;
+      break;
+    case set_sec:
+      clk_mode = set_normal;
+      break;
+    default:
+      clk_mode = normal;
+      break;
+
+  }
+}
+
+void update_next(state_t mode) {
+  switch (mode) {
+    case set_normal:
+      // DO NOTHING
+      break;
+    case set_alarm_active:
+      clk_alarm = !clk_alarm;
+      break;
+    case set_alarm_hour:
+      clk_alarm_hour = (clk_alarm_hour + 1) % 24;
+      break;
+    case set_alarm_min:
+      clk_alarm_min = (clk_alarm_min + 1) % 60;
+      break;
+    case set_hour:
+      clk_hour = (clk_hour + 1) % 24;
+      break;
+    case set_min:
+      clk_minute = (clk_minute + 1) % 60;
+      break;
+    case set_sec:
+      clk_second = 0;
+      break;
+  }
+}
+
 
 void update_time() {
   clk_second++;
